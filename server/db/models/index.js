5bb9b99a5acb5f0dd152b8d232b1f097295c0108
EdChiu-tech <edward_chiu604@gmail.com>
2021-09-06 01:31:25 -0700

    drafted models for new db design

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,18 +1,23 @@
-const Conversation = require("./conversation");
+const Conversation = require("./chatroom");
 const User = require("./user");
 const Message = require("./message");
-
+const Chatroom = require("./chatroom1");
 
 // associations
 
 User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
-Message.belongsTo(Conversation);
-Conversation.hasMany(Message);
+Chatroom.hasMany(Conversation);
+Conversation.belongsTo(User);
+Conversation.belongsTo(Chatroom);
+
+User.hasMany(Message);
+Chatroom.hasMany(Message);
+Message.belongsTo(Chatroom);
+Message.belongsTo(User);
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
+  Chatroom,
 };
--- server/db/models/message.js
+++ server/db/models/message.js
@@ -12,7 +12,7 @@ const Message = db.define("message", {
   },
   unread: {
     type: Sequelize.BOOLEAN,
-    default: true
+    default: true,
   },
 });
 
